d98ec379ebeb4a527e3d89c0972a98c0d1b6269a
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-03-18 14:15:43 -0300

    feat: Quick Planner adicionado aos projetos

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-import { FaExternalLinkAlt, FaImage } from "react-icons/fa";
+import { FaExternalLinkAlt, FaGithub, FaImage } from "react-icons/fa";
+import IconButton from "../components/IconButton";
 
 export const metadata: Metadata = {
   title: "Projetos",
@@ -13,6 +14,7 @@ const projectList = [
   {
     image: "/room-pages-desktop.png",
     url: "https://room-page-six.vercel.app",
+    repo: "room-page",
 
     name: "Room Pages",
     description: "Página estática de venda de móveis",
@@ -20,22 +22,34 @@ const projectList = [
     tags: ["Next.js", "React", "TailwindCSS"],
   },
   {
-    image: "/envclass-desktop.png",
-    url: "https://pypi.org/project/envclass",
-
-    name: "Envclass",
-    description: "Biblioteca para gerenciar variáveis de ambiente",
-
-    tags: ["Python"],
-  },
-  {
     image: "/fast-todo-desktop.png",
     url: "https://fast-todo.fly.dev/docs",
+    repo: "fast-todo",
 
     name: "Fast Todo",
     description: "API para gerenciar lista de tarefas",
 
     tags: ["Python", "FastAPI", "PostgreSQL"],
+  },
+  {
+    image: "/quick-planner-desktop.png",
+    url: "https://quick-planner.vercel.app",
+    repo: "quick-planner",
+
+    name: "Quick Planner",
+    description: "Gerador rápido de rotinas no calendário",
+
+    tags: ["Next.js", "React", "TailwindCSS", "Codemirror"],
+  },
+  {
+    image: "/envclass-desktop.png",
+    url: "https://pypi.org/project/envclass",
+    repo: "envclass",
+
+    name: "Envclass",
+    description: "Biblioteca para gerenciar variáveis de ambiente",
+
+    tags: ["Python"],
   },
 ];
 
@@ -71,29 +85,41 @@ export default function Projects() {
                 <div className="w-full h-32 lg:h-64 rounded" />
               </div>
             </Link>
-            <h2 className="text-md font-semibold mt-2">
-              {project.name}
-            </h2>
-            <p className="my-2">{project.description}</p>
-            <ul className="flex flex-wrap text-[10px]">
-              {project.tags.map((tag, index) => (
-                <li
-                  key={index}
-                  className="
-                  p-[3px]
-                  m-[3px]
-                  text-white
-                  font-semibold
-                  cursor-default
-                  bg-stone-700
-                  hover:bg-stone-800
-                  rounded
-                  transition"
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
+            <div className="flex gap-2 mt-2">
+              <div className="flex flex-col w-3/4 gap-2">
+                <h2 className="text-md font-semibold">
+                  {project.name}
+                </h2>
+                <p>{project.description}</p>
+                <ul className="flex flex-wrap text-[10px]">
+                  {project.tags.map((tag, index) => (
+                    <li
+                      key={index}
+                      className="
+                      p-[3px]
+                      m-[3px]
+                      text-white
+                      font-semibold
+                      cursor-default
+                      bg-stone-700
+                      hover:bg-stone-800
+                      rounded
+                      transition"
+                    >
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="flex justify-end items-start w-1/4">
+                <IconButton
+                  url={`http://github.com/brunodavi/${project.repo}`}
+                  icon={FaGithub}
+                  iconSize={32}
+                  className="m-1"
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
