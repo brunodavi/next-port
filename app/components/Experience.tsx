1f755ddbe39755b70dbcece9d3c47839039e6938
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-03-27 09:31:01 -0300

    refactor: página de experiencia com componente separado

---
+++ app/components/Experience.tsx
@@ -0,0 +1,59 @@
+import { IconType } from "react-icons"
+
+type Experience = {
+  name: string,
+  description: string,
+  achievements: string[],
+  start: string,
+  end: string,
+}
+
+type PropType = {
+  icon: IconType,
+  title: string,
+  experienceList: Experience[],
+}
+
+export default function Experience(props: PropType) {
+  return (
+    <div>
+      <div className="mx-5 my-10">
+        <h1 className="text-3xl font-bold">{props.title}</h1>
+      </div>
+      <div className="mx-5">
+        {props.experienceList.map((experience: Experience) => (
+          <div key={experience.name} className="my-5">
+            <div>
+              <div className="flex items-center">
+                <props.icon />
+                <h1 className="ml-2 font-bold">
+                  {experience.name}
+                </h1>
+              </div>
+              <div className="pl-6">
+                <div>
+                  <span>{experience.start}</span>
+                  {" - "}
+                  <span>{experience.end}</span>
+                </div>
+                <p className="w-96">{experience.description}</p>
+                <div className="mt-2">
+                  {
+                    (experience.achievements.length > 0) && (
+                      <p className="font-bold">Conquistas:</p>
+                    )
+                  }
+                  <ul className="list-inside list-disc">
+                    {experience.achievements.map((achievement, index) => (
+                      <li key={index}>{achievement}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,108 +1,67 @@
 import { Metadata } from "next";
 import { MdOutlineWork, MdSchool } from "react-icons/md";
 
+import Experience from "../components/Experience";
+
 export const metadata: Metadata = {
   title: "Experiência",
-  description: "O que aprendi com meus trabalhos",
+  description: "O que aprendi com meus trabalhos.",
 };
 
-export default function Experience() {
-  const experienceList = [
-    {
-      name: "Conveste",
-      achievements: [
-        "Otimizações de APIs",
-        "Melhoria seguindo o UI/UX",
-        "Cultura de Testes",
-      ],
-      dataEntrada: "Abril 2022",
-      dataSaida: "Abril 2023",
-    },
-  ];
+const experienceList = [
+  {
+    name: "Conveste",
+    description:
+      "Participei do desenvolvimento de uma aplicação de BI"
+      + " da concepção até a entrada em produção, impactando a"
+      + " entrega da companhia de forma positiva e gerando insights estratégicos.",
+    achievements: [
+      "Otimizações de APIs",
+      "Melhoria seguindo o UI/UX",
+      "Cultura de Testes",
+    ],
+    start: "Abril 2022",
+    end: "Abril 2023",
+  },
+];
 
-  const educationList = [
-    {
-      name: "Trybe",
-      achievements: [
-        "Metodologias Ágeis",
-        "Testes Unitários",
-        "Desenvolvimento Full Stack",
-      ],
-      dataEntrada: "Maio 2021",
-      dataSaida: "Dezembro 2022",
-    },
-  ];
+const educationList = [
+  {
+    name: "CS50",
+    description: "Curso de ciência da computação de Harvard.",
+    achievements: [],
+    start: "Março 2024",
+    end: "Até o momento",
+  },
+  {
+    name: "Trybe",
+    description:
+      "Escola de Desenvolvimento Full Stack com projetos reais"
+      + " realizados individualmente ou em equipes, seguindo"
+      + " as melhores práticas do mercado.",
+    achievements: [
+      "Metodologias Ágeis",
+      "Testes Unitários",
+      "Desenvolvimento Full Stack",
+    ],
+    start: "Maio 2021",
+    end: "Dezembro 2022",
+  },
+];
 
+export default function ExperiencePage() {
   return (
     <div>
-      <div>
-        <div className="mx-5 my-10">
-          <h1 className="text-3xl font-bold">Experiência</h1>
-        </div>
-        <div className="mx-5">
-          {experienceList.map((experience, index) => (
-            <div key={index} className="my-5">
-              <div>
-                <div className="flex items-center">
-                  <MdOutlineWork />
-                  <h1 className="ml-2 font-bold">
-                    {experience.name}
-                  </h1>
-                </div>
-                <div className="pl-6">
-                  <div>
-                    <span>{experience.dataEntrada}</span>
-                    {" - "}
-                    <span>{experience.dataSaida}</span>
-                  </div>
-                  <div className="mt-2">
-                    <p className="font-bold">Conquistas:</p>
-                    <ul className="list-inside list-disc">
-                      {experience.achievements.map((achievement, index) => (
-                        <li key={index}>{achievement}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div>
-        <div className="mx-5 my-10">
-          <h1 className="text-3xl font-bold">Educação</h1>
-        </div>
-        <div className="mx-5">
-          {educationList.map((education, index) => (
-            <div key={index} className="my-5">
-              <div>
-                <div className="flex items-center">
-                  <MdSchool />
-                  <h1 className="ml-2 font-bold">
-                    {education.name}
-                  </h1>
-                </div>
-                <div className="pl-6">
-                  <div>
-                    <span>{education.dataEntrada}</span>
-                    {" - "}
-                    <span>{education.dataSaida}</span>
-                  </div>
-                  <div className="mt-2">
-                    <p className="font-bold">Conquistas:</p>
-                    <ul className="list-inside list-disc">
-                      {education.achievements.map((achievement, index) => (
-                        <li key={index}>{achievement}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Experience
+        icon={MdOutlineWork}
+        title="Experiência"
+        experienceList={experienceList}
+      />
+      <Experience
+        icon={MdSchool}
+        title="Educação"
+        experienceList={educationList}
+      />
     </div>
   );
 }
